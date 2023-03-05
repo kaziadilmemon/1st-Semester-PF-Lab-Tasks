0180df7eb778c2cfdae34e12e5728577516bf286
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 13:48:01 +0500

    Create Task 10.cpp

---
+++ Lab 4/Task 10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,k;
+	cout<<"enter the first number"<<endl;
+	cin>>i;
+	cout<<"enter the second number"<<endl;
+	cin>>j;
+	cout<<"enter the third number"<<endl;
+	cin>>k;
+	if(i==j&&j==k&&i==k){
+	
+	cout<<"all three numbers are equal"<<endl;}
+	else cout<<"no any further calculation"<<endl;
+	return 0;
+	
+}
