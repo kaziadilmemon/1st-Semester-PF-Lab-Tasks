015c1f8f4bc084d33a9b123d4c65487d6dc0bd65
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:02:38 +0500

    Update Task 1.cpp

--- Lab 7/Task 1.cpp
+++ Lab 7/Task 1.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+int largernum(int num1,int num2){
+	if(num1>num2){
+		cout<<"first number is greater : "<<num1<<endl;
+	}
+	if(num2>num1){
+		cout<<"second number is greater : "<<num2<<endl;
+	}
+	return num1,num2;
+}
+int main(){
+	int a,b;
+	cout<<"enter the first number"<<endl;
+	cin>>a;
+	cout<<"enter the second number"<<endl;
+	cin>>b;
+	largernum(a,b);
+	return 0;
+}
