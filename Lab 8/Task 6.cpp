9387c222568b43562846e86c712fac7da4f47ad7
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:36:22 +0500

    Update Task 6.cpp

--- Lab 8/Task 6.cpp
+++ Lab 8/Task 6.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+   int arr[] = { 5, 2, 7, 1, 8, 3 };
+   int n = sizeof(arr) / sizeof(arr[0]);
+   int smallest = arr[0], index = 0;
+   for (int i = 1; i < n; i++) {
+      if (arr[i] < smallest) {
+         smallest = arr[i];
+         index = i;
+      }
+   }
+   cout << "Smallest element is : " << smallest << endl;
+   cout << "Index of smallest element: " << index << endl;
+   return 0;
+}
