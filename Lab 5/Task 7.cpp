ff0ba3b02f6c9d8ad6fe552d77978dc2e8099cff
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 17:39:17 +0500

    Update Task 7.cpp

--- Lab 5/Task 7.cpp
+++ Lab 5/Task 7.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+	float sum=0;
+	float average;
+	int i;
+	for( i=0;i<=100;i++){
+		sum+=i;
+	
+	}
+		cout<<sum<<endl;
+	average=sum/100;
+	cout<<average;
+	return 0;
+}
