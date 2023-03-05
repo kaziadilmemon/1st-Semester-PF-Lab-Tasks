76706b59a6ec915b5cda264716fe692ffcdb2d91
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 13:43:08 +0500

    Update Task 3.cpp

--- Lab 4/Task 3.cpp
+++ Lab 4/Task 3.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main() {
+	int i,j,k;
+	cout<<"enter the first number"<<endl;
+	cin>>i;
+	cout<<"enter the second number"<<endl;
+	cin>>j;
+	cout<<"enter the third number"<<endl;
+	cin>>k;
+	if(i<j&&j<k){cout<<"increasing order"<<endl;}
+	else if(i>j&&j>k){cout<<"decreasing order"<<endl;}
+	else cout<<"no further calculation"<<endl;
+	return 0;
+	
+}
