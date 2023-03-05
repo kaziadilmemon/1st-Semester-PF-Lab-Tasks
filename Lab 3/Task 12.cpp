17e72934b687a8d775a294fd9f3059a376651d84
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 12:57:00 +0500

    Create Task 12.cpp

---
+++ Lab 3/Task 12.cpp
@@ -0,0 +1,9 @@
+#include "iostream"
+using namespace std;
+int main(){
+	int x=5;
+	int y=10;
+	int z=x+y;//here we put  declearation of variable is missing 
+	cout<<z;//here we have to call var z
+	return 0;
+}
