58e18104a2c48dc729dffdf702bb058e9350e78a
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 16:20:23 +0500

    Create Task 2.cpp

---
+++ Lab 5/Task 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<" odd  even  "<<endl;
+	for(int i=1;i<=20;i++){
+		if(i%2==1)
+		cout<<i<<endl; 
+		else 
+		cout<<"\t"<<i<<endl;
+	}
+	return 0;
+	
+
+}
