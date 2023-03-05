afceecb343f371178e5059511f2751fd7c4a09e8
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:04:18 +0500

    Update Task 5.cpp

--- Lab 7/Task 5.cpp
+++ Lab 7/Task 5.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
+void fun(int& i){
+	if(i>4){
 
+cout<<"final i = "<<i;
+			}
+	else{
+	i++;
+	fun(i);
+	}
+}
+int main(){
+	int i=1;
+cout<<"intial i = "<<i<<endl;
+	fun(i);
+}
