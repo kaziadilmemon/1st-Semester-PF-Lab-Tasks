24d8b1f7f9eb811fb87d2463d41bcc97e3015d4d
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:03:29 +0500

    Update Task 3.cpp

--- Lab 7/Task 3.cpp
+++ Lab 7/Task 3.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+using namespace std;
+int swap(int a ,int b){
+	cout<<"the first number before "<<a<<"the second number before "<<b<<endl;
+	int c;
+	c=a;
+	a=b;
+	b=c;
+	cout<<"the first number after "<<a<<"the second number after "<<b<<endl;
+	return a,b;
+}
+int main(){
+	int a=7;
+	int b=9;
+	swap(a,b);
+	return 0;
+}
