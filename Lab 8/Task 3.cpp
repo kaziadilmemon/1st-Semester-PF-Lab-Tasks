6ca77538027105d6f4ec358a9ad7417c1a97e8f9
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:33:39 +0500

    Update Task 3.cpp

--- Lab 8/Task 3.cpp
+++ Lab 8/Task 3.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[10] = {1, 2, 3, 5, 8, 10, 12, 23, 28, 15};
+    int searchValue;
+    bool isFound = false;
+
+    cout << "Enter an integer value to search for: ";
+    cin >> searchValue;
+
+    for (int i = 0; i < 10; i++) {
+        if (array[i] == searchValue) {
+            isFound = true;
+            break;
+        }
+    }
+
+    if (isFound) {
+        cout << "Number is found" << endl;
+    } else {
+        cout << "Number does not found" << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
 
