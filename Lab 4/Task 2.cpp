e610f0369ba47a62bd653d775cfd6f6649432a58
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 13:42:42 +0500

    Update Task 2.cpp

--- Lab 4/Task 2.cpp
+++ Lab 4/Task 2.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int main() {
+	int Tclassheld;cout<<"Total classes"<<endl;
+	cin>>Tclassheld;
+	int classattend;cout<<"classes held"<<endl;
+	cin>>classattend;
+	float per=((float)classattend/Tclassheld*100);
+	cout<<"show your percentage"<<per<<endl;
+	if(per>=75){cout<<"you can sit in exam"<<endl;
+	}
+	else cout<<"you cannot sit in exam"<<endl;
+	return 0;
+}
