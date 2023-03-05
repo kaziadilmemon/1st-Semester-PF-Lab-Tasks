2c348a03d0ee7b111de2ebe9c088bda26d4a920b
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 13:42:10 +0500

    Update Task 1.cpp

--- Lab 4/Task 1.cpp
+++ Lab 4/Task 1.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	int age;cout<<"tell me your age"<<endl;
+	cin>>age;
+	if(age>=18){ cout<<"(new) you are eligible to vote"<<endl;}
+	 //else if (age>18) {
+	//cout<<"you are eligible to vote"<<endl;}
+	else cout<<"you are not eligible to vote"<<endl;
+	return 0; }
 
