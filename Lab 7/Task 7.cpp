9c6c4dfdd20d80427f38089e19c6768281884e5d
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:05:09 +0500

    Update Task 7.cpp

--- Lab 7/Task 7.cpp
+++ Lab 7/Task 7.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+int i=0;
+int username(string name){
+	if(i<name.length()){
+		cout<<name[i]<<endl;
+		i++;
+		username(name);
+	}
+	else 
+	return 1;
+	}
+	int main(){
+	string b;
+	cout<<"enter  name"<<endl;
+	cin>>b;
+	username(b);
+	return 0;	
+	
+}
