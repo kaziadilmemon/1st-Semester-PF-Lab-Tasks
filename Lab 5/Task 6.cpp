1daf0c11797848fd87216fd75b2c586b0d58da11
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 17:38:54 +0500

    Update Task 6.cpp

--- Lab 5/Task 6.cpp
+++ Lab 5/Task 6.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+	int count=0;
+	char a,b;
+	cin>>a>>b;
+	for(int i=a+1;i<b;i++){
+		char c=i;
+		cout<<c<<" ";
+		count++; 
+	}
+	cout<<endl<<" total alphabets numbers"<<count;
+	return 0;
+}
