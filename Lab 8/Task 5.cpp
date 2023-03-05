4551ad4a5b0a03d4fe23586a966444a8c6e2b908
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:34:27 +0500

    Update Task 5.cpp

--- Lab 8/Task 5.cpp
+++ Lab 8/Task 5.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
 
+using namespace std;
+
+int main(){
+	int size=5;
+	int arr[size]={1,2,3,4,5};
+	int sum=0;
+	int pro=1;
+	for(int i=size-1;i>size-3;i--){
+		cout<<arr[i]<<endl;
+		sum+=arr[i];
+		pro*=arr[i];
+	}
+	cout<<sum<<endl<<pro;
+	return 0;
+}
