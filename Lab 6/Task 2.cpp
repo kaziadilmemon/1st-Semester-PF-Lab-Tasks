78f6a86684e7fcf00ee66c09d21eef659aa7d681
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 17:55:32 +0500

    Update Task 2.cpp

--- Lab 6/Task 2.cpp
+++ Lab 6/Task 2.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+struct student{
+	int studentsobtainedmarks;
+	float studenttotalmarks;
+};
+int main(){
+	student ali;
+	ali.studenttotalmarks;
+	cout<<"ali total marks "<<endl;
+	cin>>ali.studenttotalmarks;
+	ali.studentsobtainedmarks;
+	cout<<"ali obtaiend marks "<<endl;
+	cin>>ali.studentsobtainedmarks;
+	float per=(ali.studentsobtainedmarks/ali.studenttotalmarks*100);
+	cout<<" ali percentage "<<per<<endl;
+	return 0;
+	
+	
+	
+}
 
