147382d8c4b35ab63d0bc0568065d745b8d1c347
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:34:04 +0500

    Update Task 4.cpp

--- Lab 8/Task 4.cpp
+++ Lab 8/Task 4.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+using namespace std;
+int main(){ 
+ int array[10]={1,2,3,5,8,10,12,23,28,15};
+ for(int i=0;i<10;i++){
+ 	if(array[i]%2==1){
+ 		cout<<array[i]<<endl;
+	 }
+}
+ 	return 0;
+ }
