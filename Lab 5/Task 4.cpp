f60d44bcca74fe083687e2aaa4aa6fd32ebc0b68
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 16:21:27 +0500

    Update Task 4.cpp

--- Lab 5/Task 4.cpp
+++ Lab 5/Task 4.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
 
+using namespace std;
+int main() {
+	char c;
+	cout<<"vowels	consonants"<<endl;
+	for(  c=65;c<=90;c++){
+		if(c==65||c==69||c==73||c==79||c==85)
+		{
+		cout<<c;
+		}
+		else 
+		 {
+		 cout<<"	"<<c<<endl;
+	      }
+	}
+	return 0;
+}
