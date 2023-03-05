2ba28674417420843397e4dc86be9ad8951a3504
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 17:55:04 +0500

    Update Task 1.cpp

--- Lab 6/Task 1.cpp
+++ Lab 6/Task 1.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+struct employee{
+	int employeeno;
+	float employeesalary;
+};
+int main(){
+	employee saif;
+	saif.employeeno=03324674206;
+	saif.employeesalary=5000000;
+	cout<<"saif employee number "<<saif.employeeno<<endl;
+	cout<<"saif employeesalary  "<<saif.employeesalary<<endl;
+	return 0;
+}
