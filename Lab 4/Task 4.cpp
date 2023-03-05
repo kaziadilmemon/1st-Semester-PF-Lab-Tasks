2733fb6842a749ce7c67bc07405ebb6b8b57a162
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 13:43:37 +0500

    Update Task 4.cpp

--- Lab 4/Task 4.cpp
+++ Lab 4/Task 4.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+using namespace std ;
+int main ()
+{
+	char c;
+	cout<<"enter the character"<<endl;
+	cin>>c;
+	if(c>=65&&c<=90)
+	cout<<"upper case";
+	else
+	cout<<"lower case";
+	return 0;
+}
