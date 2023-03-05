b0427d0be6e9c94422932484a40411524414b927
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 13:49:06 +0500

    Create Task 12.cpp

---
+++ Lab 4/Task 12.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main() 
+{ 
+      char vowel;
+	cout<<"enter the character"<<endl;
+	cin>>vowel;
+	switch (vowel)
+	
+	{
+		case('a'):
+		cout<<"vowel"<<endl;break;
+		case('e'):
+		cout<<"vowel"<<endl;break;
+		case('i'):
+		cout<<"vowel"<<endl;break;
+		case('o'):
+		cout<<"vowel"<<endl;break;
+		case('u'):
+		cout<<"vowel"<<endl;break;
+		default:`
+		cout<<"consonant"<<endl;break;
+	}
+	return 0;
+		
+		 
+	
+} 
