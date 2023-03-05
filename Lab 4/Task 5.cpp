f937947e80f4e0a6e5072233653b0907f2194a7e
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 13:44:16 +0500

    Update Task 5.cpp

--- Lab 4/Task 5.cpp
+++ Lab 4/Task 5.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	string firstword;
+	cout<<"enter the word"<<endl;
+	cin>>firstword;
+	cout<<"first word is "<<firstword;
+	return 0;
+}
