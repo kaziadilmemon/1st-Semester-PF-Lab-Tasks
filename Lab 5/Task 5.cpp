4a7f9a5d7c56c78ddd315651c12f34e20a5c5295
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 17:38:27 +0500

    Update Task 5.cpp

--- Lab 5/Task 5.cpp
+++ Lab 5/Task 5.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main() {
+	char a;
+	for(a=97;a<=122;a++){
+		cout<<a<<" ";
+		if(a==106){
+			cout<<endl;
+		}
+		else if(a==116){
+			cout<<endl;
+		}
+		 
+	} 
+	return 0;
+}
