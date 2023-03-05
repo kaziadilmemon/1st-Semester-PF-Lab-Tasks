40f545acb0cb68b86c0d10e05b3e0124cdc73714
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:03:03 +0500

    Update Task 2.cpp

--- Lab 7/Task 2.cpp
+++ Lab 7/Task 2.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+void zerosmaller(int &num1,int &num2){
+	if(num1<num2){
+		num1=0;
+	}
+	if(num2<num1){
+		num2=0;
+	}
+}
+int main(){
+	int num1,num2;
+	cout<<" enter first numeber"<<endl;
+	cin>>num1;
+	cout<<"enter second number"<<endl;
+	cin>>num2;
+	zerosmaller(num1,num2);
+	cout<<"num1 = "<<num1<<endl<<"num2 = "<<num2<<endl;
+	
+	return 0;
+}
