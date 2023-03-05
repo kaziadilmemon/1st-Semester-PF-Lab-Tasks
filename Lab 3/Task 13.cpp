74826f3695d60d61719e3b6e87e88ba56e3bb3fc
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 12:57:28 +0500

    Create Task 13.cpp

---
+++ Lab 3/Task 13.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace std;
+int main(){
+cout<<"         C \n"
+    <<"       i   I \n"
+    <<"      s     s \n"
+    <<"     b       b \n"
+    <<"    e         e \n"
+    <<"   s           s \n"
+    <<"   tsebsicisbest \n";
+
+		return 0;
+}
