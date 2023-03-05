7e85b67e21e54a51fcd286064f479fcc3a9156f9
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 18:04:44 +0500

    Update Task 6.cpp

--- Lab 7/Task 6.cpp
+++ Lab 7/Task 6.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int swap(int&n1,int&n2){
+	int n3=n1;
+	n1=n2;
+	n2=n3;
+	
+}
+int main(){
+	int n1=45;
+	int n2=56;
+	cout<<"the value of n1 and n2 before respectively"<<n1<<"  "<<n2<<endl;
+	swap(n1,n2);
+	cout<<"the value of n1 and n2 after respectively"<<n1<<"  "<<n2<<endl;
+	return 0;
+}
