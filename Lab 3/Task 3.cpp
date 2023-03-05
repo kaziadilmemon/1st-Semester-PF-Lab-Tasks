314b1d8c3c96dd96f18db4c7cde085f17e249234
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-05 12:49:27 +0500

    Update Task 3.cpp

--- Lab 3/Task 3.cpp
+++ Lab 3/Task 3.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Size of INT is "<<sizeof(int)<<" bytes"<<endl
+		<<"Size of FLOAT is "<<sizeof(float)<<" bytes"<<endl
+		<<"Size of BOOlIAN is "<<sizeof(bool)<<" byte"<<endl
+		<<"Size of LONG is "<<sizeof(long)<<" bytes"<<endl
+		<<"Size of DOUBLE is "<<sizeof(double)<<" bytes"<<endl
+		<<"Size of CHARACTERS is "<<sizeof(char)<<" byte"<<endl;
+	return 0;
+}
 
