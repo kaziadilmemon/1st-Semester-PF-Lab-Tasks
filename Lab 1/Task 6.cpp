af6cb2469740669cb43e525fab5ee1bf003fa7d1
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-07 10:37:26 +0500

    Update Task 6.cpp

--- Lab 1/Task 6.cpp
+++ Lab 1/Task 6.cpp
@@ -1,1 +1,17 @@
+// Task.6 
+// Write three C++ statements to print the asterisk pattern as 
+// shown below.
 
+//	*****
+//	*****
+//	*****
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "*****" << "\n"
+		 << "*****" << "\n"
+		 << "*****" << "\n";
+	return 0;
+}
