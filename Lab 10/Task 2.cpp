f5eee7c820dc7c1bf69a3a9ac6f4fc7553b2b732
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-07 19:20:08 +0500

    Update Task 2.cpp

--- Lab 10/Task 2.cpp
+++ Lab 10/Task 2.cpp
@@ -1,1 +1,20 @@
+/*Create a class called MyClass that has one int member. Include member functions to 
+initialize it to 0,
+to initialize it to an integer value, to display it. Write a program to test this class*/
+#include<iostream>
+Using namespace std;
+class MyClass{
+public:
+int member;
+	int zero(){
+	member=0;
+	cout<<"when initialize to zero:"<<member<<endl;}
+int value(int a){
+member =a;
+cout<<"the initialize value:"<<member;}
+};
+int main()
+{MyClass test;
+test.zero();
+test.value(2);}
 
