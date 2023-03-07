1037f667d9448bbeab3d8f90c4bf7034174d0aeb
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-07 10:36:24 +0500

    Update Task 4.cpp

--- Lab 1/Task 4.cpp
+++ Lab 1/Task 4.cpp
@@ -1,1 +1,18 @@
+// Task.4 
+// Write a C++ program to declare an integer variable Hours and
+// assign it a value of your choice and convert Hours in terms of minutes.
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	int hours;
+	
+	cout << "Enter time in hours: ";
+	cin >> hours;
+	
+	hours = (hours * 60);
+	
+	cout << "Time in minutes: " << hours;
+	return 0;
+}
