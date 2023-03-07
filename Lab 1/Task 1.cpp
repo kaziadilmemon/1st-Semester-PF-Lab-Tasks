27751f45f2405253223215fb12fbe57b8e93a6a9
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-07 10:34:48 +0500

    Update Task 1.cpp

--- Lab 1/Task 1.cpp
+++ Lab 1/Task 1.cpp
@@ -1,1 +1,16 @@
+// Task.1
+// Write a C++ program to print the following lines:
+//		 You are 10 years old.
+//		 You are too young to play the game (Hint use an int variable for age) .
 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age;
+	age = 10;
+	cout << "You are " << age << " years old" << endl;
+	cout << "You are too young to play the game";
+	return 0;
+}
