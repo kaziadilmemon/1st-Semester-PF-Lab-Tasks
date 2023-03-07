964bb9e91b206d4527173f5d8ef7c315c10e5f64
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-07 10:40:36 +0500

    Create Task 9.cpp

---
+++ Lab 1/Task 9.cpp
@@ -0,0 +1,25 @@
+// Task. 13
+// Write a program to print the following pattern:
+
+//                        C
+//                      i   I
+//                    s       s
+//                  b           b
+//                e               e
+//              s                   s
+//            t s e b s i C i s b e s t
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "           C				" << endl;
+	cout << "         i   I				" << endl;
+	cout << "       s       s			" << endl;
+	cout << "      b           b		" << endl;
+	cout << "    e               e		" << endl;
+	cout << "  s                   s	" << endl;
+	cout << " t s e b s i C i s b e s t " << endl;
+
+}
