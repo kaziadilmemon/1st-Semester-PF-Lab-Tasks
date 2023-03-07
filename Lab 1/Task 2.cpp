03b3f299dfd79b6a28590d3eb122511714531132
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-07 10:35:15 +0500

    Update Task 2.cpp

--- Lab 1/Task 2.cpp
+++ Lab 1/Task 2.cpp
@@ -1,1 +1,24 @@
+// Task.2 
+// Write a program in C++ to print the sum of two numbers. 
+//			Sample Output:
+//			-----------------------------------
+//			The sum of 29 and 30 is : 59
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	int y;
+	int z;
+	
+	cout << "Enter 1st number: ";
+	cin >> x;
+	
+	cout << "Enter 2nd number: ";
+	cin>> y;
+	
+	z = x + y;
+	cout << "The sum of " << x << " and " << y << " is: " << z;
+	return 0;
+}
