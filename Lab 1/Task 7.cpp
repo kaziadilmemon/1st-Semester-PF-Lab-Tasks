9f5484a66aa9774c6d26233cffe6bb108173afce
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-07 10:38:03 +0500

    Update Task 7.cpp

--- Lab 1/Task 7.cpp
+++ Lab 1/Task 7.cpp
@@ -1,1 +1,12 @@
+// Task.7 
+// Write a program in C++ to check the upper and lower limits of 
+// integer.
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Lower case limit of an integer: " << INT_MIN << endl;
+	cout << "Upper case limit of an integer: " << INT_MAX << endl;
+	return 0;
+}
