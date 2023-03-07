bb06a1d3fca39b2897698d38e84bef4f7c92401a
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-07 10:36:53 +0500

    Update Task 5.cpp

--- Lab 1/Task 5.cpp
+++ Lab 1/Task 5.cpp
@@ -1,1 +1,23 @@
+// Task.5
+// Write a C++ program to declare two integers, one float 
+// variables and assign 10, 15, and 12.6 to them respectively. It then 
+// prints these values on the screen.
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	int y;
+	float z;
+	
+	x = 10;
+	y = 15;
+	z = 12.6;
+	
+	cout << x << endl;
+	cout << y << endl;
+	cout << z << endl;
+	
+	return 0;
+}
